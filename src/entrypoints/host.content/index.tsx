38cab001134910d0c7c65734f0603189ca3c8c58
Kuiliang Zhang <z1219202167@outlook.com>
2025-05-24 23:32:48 -0700

    ci: test include eruda

--- src/entrypoints/host.content/index.tsx
+++ src/entrypoints/host.content/index.tsx
@@ -1,3 +1,4 @@
+import eruda from 'eruda'
 import { loadGlobalConfigPromise } from '@/utils/config/config'
 import { registerTranslationTriggers } from './translation-trigger'
 import './style.css'
@@ -6,7 +7,7 @@ export default defineContentScript({
   matches: ['*://*/*'],
   async main() {
     await loadGlobalConfigPromise
-    // eruda.init()
+    eruda.init()
     registerTranslationTriggers()
   },
 })
