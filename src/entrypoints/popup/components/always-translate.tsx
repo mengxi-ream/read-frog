c585d92f21a812cc7e7fc796eae3ba7508574727
Kuiliang Zhang <z1219202167@outlook.com>
2025-05-26 15:21:55 -0700

    i18n: always translation switch for popup

--- src/entrypoints/popup/components/always-translate.tsx
+++ src/entrypoints/popup/components/always-translate.tsx
@@ -16,7 +16,7 @@ export function AlwaysTranslate() {
   return (
     <div className="flex items-center justify-between gap-2">
       <span className="text-[13px] font-medium">
-        Always translate this site
+        {i18n.t('popup.alwaysTranslate')}
       </span>
       <Switch
         checked={isCurrentSiteInPatterns}
