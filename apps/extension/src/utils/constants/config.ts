cda583fe21a67fce03096cbdfcf32f02446c065e
ananaBMaster <68643891+ananaBMaster@users.noreply.github.com>
2025-08-11 16:12:54 -0700

    fix: migrate config

--- apps/extension/src/utils/constants/config.ts
+++ apps/extension/src/utils/constants/config.ts
@@ -14,7 +14,7 @@ import { DEFAULT_REQUEST_CAPACITY, DEFAULT_REQUEST_RATE } from './translate'
 import { DEFAULT_TRANSLATION_NODE_STYLE } from './translation-node-style'
 
 export const CONFIG_STORAGE_KEY = 'config'
-export const CONFIG_SCHEMA_VERSION = 13
+export const CONFIG_SCHEMA_VERSION = 14
 
 export const DEFAULT_PROVIDER_CONFIG: ProvidersConfig = {
   openai: {
