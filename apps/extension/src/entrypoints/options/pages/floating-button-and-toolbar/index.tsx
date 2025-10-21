5df041f19bba99d035bb9430fec3ab079dcfda9a
ananaBMaster <68643891+ananaBMaster@users.noreply.github.com>
2025-10-21 15:22:16 -0700

    i18n: update i18n

--- apps/extension/src/entrypoints/options/pages/floating-button-and-toolbar/index.tsx
+++ apps/extension/src/entrypoints/options/pages/floating-button-and-toolbar/index.tsx
@@ -2,9 +2,9 @@ import { i18n } from '#imports'
 import { PageLayout } from '../../components/page-layout'
 import { FloatingButtonDisabledSites } from './floating-button-disabled-sites'
 
-export function FloatingButtonPage() {
+export function FloatingButtonAndToolbarPage() {
   return (
-    <PageLayout title={i18n.t('options.floatingButton.title')} innerClassName="[&>*]:border-b [&>*:last-child]:border-b-0">
+    <PageLayout title={i18n.t('options.floatingButtonAndToolbar.title')} innerClassName="[&>*]:border-b [&>*:last-child]:border-b-0">
       <FloatingButtonDisabledSites />
     </PageLayout>
   )
