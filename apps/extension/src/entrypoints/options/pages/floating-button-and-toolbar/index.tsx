51371842875517a97901feddfdb9d7bc4bb600b2
ananaBMaster <68643891+ananaBMaster@users.noreply.github.com>
2025-10-21 15:28:41 -0700

    feat: disable floating button in option

--- apps/extension/src/entrypoints/options/pages/floating-button-and-toolbar/index.tsx
+++ apps/extension/src/entrypoints/options/pages/floating-button-and-toolbar/index.tsx
@@ -1,10 +1,12 @@
 import { i18n } from '#imports'
 import { PageLayout } from '../../components/page-layout'
 import { FloatingButtonDisabledSites } from './floating-button-disabled-sites'
+import { FloatingButtonGlobalToggle } from './floating-button-global-toggle'
 
 export function FloatingButtonAndToolbarPage() {
   return (
     <PageLayout title={i18n.t('options.floatingButtonAndToolbar.title')} innerClassName="[&>*]:border-b [&>*:last-child]:border-b-0">
+      <FloatingButtonGlobalToggle />
       <FloatingButtonDisabledSites />
     </PageLayout>
   )
