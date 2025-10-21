f33b86f4fa55fece34b01157673600cabff95bcf
ananaBMaster <68643891+ananaBMaster@users.noreply.github.com>
2025-10-21 16:05:58 -0700

    feat: add floating button demo

--- apps/extension/src/entrypoints/options/pages/floating-button-and-toolbar/index.tsx
+++ apps/extension/src/entrypoints/options/pages/floating-button-and-toolbar/index.tsx
@@ -1,13 +1,17 @@
 import { i18n } from '#imports'
+import floatingButtonDemoImage from '@/assets/demo/floating-button.png'
 import { PageLayout } from '../../components/page-layout'
 import { FloatingButtonDisabledSites } from './floating-button-disabled-sites'
 import { FloatingButtonGlobalToggle } from './floating-button-global-toggle'
+import { SelectionToolbarGlobalToggle } from './selection-toolbar-global-toggle'
 
 export function FloatingButtonAndToolbarPage() {
   return (
-    <PageLayout title={i18n.t('options.floatingButtonAndToolbar.title')} innerClassName="[&>*]:border-b [&>*:last-child]:border-b-0">
+    <PageLayout title={i18n.t('options.floatingButtonAndToolbar.title')}>
+      <img src={floatingButtonDemoImage} alt="Floating Button Demo" className="w-20 h-auto mx-auto" />
       <FloatingButtonGlobalToggle />
       <FloatingButtonDisabledSites />
+      <SelectionToolbarGlobalToggle />
     </PageLayout>
   )
 }
