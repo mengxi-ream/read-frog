1898a9ab7ab300966535ec0824e77178c8d6ef38
taiiiyang <781547101@qq.com>
2025-10-23 11:17:16 +0800

    chore: extract computed logic that calculate period start day

--- apps/extension/src/hooks/use-batch-request-record.ts
+++ apps/extension/src/hooks/use-batch-request-record.ts
@@ -3,23 +3,38 @@ import { getRangeBatchRequestRecords } from '@/utils/batch-request-record'
 
 const RECENT_DAYS = [5, 7, 30, 60] as const
 
+function previousPeriodStartDay(daysBack: number) {
+  // For example, the previous period for 4 days ago is from 9 days ago to 5 days ago
+  return daysBack * 2 + 1
+}
+
+function currentPeriodStartDay(daysBack: number) {
+  return daysBack + 1
+}
+
 export function useBatchRequestRecords(currentDaysBack: number) {
   const currentPeriodQueries = useQueries({
-    queries: RECENT_DAYS.map((days) => {
-      const daysBack = days - 1
+    queries: RECENT_DAYS.map((day) => {
+      // day is a period that includes today, so after conversion it represents day - 1 days ago
+      // i.e., a period of 5 days represents 4 days ago
+      // The default end date is 0 days ago
+      const daysBack = day - 1
       return {
         queryKey: ['current-period-records', daysBack],
-        queryFn: () => getRangeBatchRequestRecords(daysBack + 1),
+        queryFn: () => getRangeBatchRequestRecords(currentPeriodStartDay(daysBack)),
       }
     }),
   })
 
   const previousPeriodQueries = useQueries({
-    queries: RECENT_DAYS.map((days) => {
-      const daysBack = days - 1
+    queries: RECENT_DAYS.map((day) => {
+      const daysBack = day - 1
       return {
         queryKey: ['previous-period-records', daysBack],
-        queryFn: () => getRangeBatchRequestRecords(daysBack * 2 + 1, daysBack + 1),
+        queryFn: () => getRangeBatchRequestRecords(
+          previousPeriodStartDay(daysBack),
+          currentPeriodStartDay(daysBack),
+        ),
       }
     }),
   })
