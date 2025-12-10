62a7596ee63137553946399ff1c20f1f52ae07fd
Yukiniro <yukiniro@hotmail.com>
2025-12-10 21:58:45 +0800

    chore: add comment

--- src/entrypoints/selection.content/selection-toolbar/index.tsx
+++ src/entrypoints/selection.content/selection-toolbar/index.tsx
@@ -73,6 +73,7 @@ export function SelectionToolbar() {
         const selection = window.getSelection()
         const selectedText = selection?.toString().trim() || ''
 
+        // https://github.com/mengxi-ream/read-frog/issues/547
         if (!isInputOrTextarea && !selection?.containsNode(e.target as Node, true) && e.target instanceof HTMLButtonElement) {
           return
         }
