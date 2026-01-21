5de2c99405c9cc9a12532394389b1550cb272def
taiiiyang <781547101@qq.com>
2026-01-21 13:47:32 +0800

    fix: remove redundant variables

--- src/entrypoints/subtitles.content/ui/subtitles-container.tsx
+++ src/entrypoints/subtitles.content/ui/subtitles-container.tsx
@@ -4,14 +4,14 @@ import { StateMessage } from './state-message'
 import { SubtitlesView } from './subtitles-view'
 
 export function SubtitlesContainer() {
-  const { subtitle, stateData, isVisible } = useAtomValue(subtitlesDisplayAtom)
+  const { stateData, isVisible } = useAtomValue(subtitlesDisplayAtom)
   const currentBlockCompleted = useAtomValue(currentBlockCompletedAtom)
 
   if (!isVisible) {
     return null
   }
 
-  if (subtitle || currentBlockCompleted) {
+  if (currentBlockCompleted) {
     return <SubtitlesView />
   }
 
