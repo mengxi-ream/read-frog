28bf0dfcc9c631b803100f6071a7aa7d41fd5770
Yiou Li <oneseagull.lii@gmail.com>
2026-05-16 17:20:54 -0700

    fix: reorder imports in floating-button-side.tsx

--- src/entrypoints/options/pages/floating-button/floating-button-side.tsx
+++ src/entrypoints/options/pages/floating-button/floating-button-side.tsx
@@ -1,6 +1,6 @@
 import type { FloatingButtonSide as FloatingButtonSideValue } from "@/types/config/floating-button"
-import { i18n } from "#imports"
 import { useAtom } from "jotai"
+import { i18n } from "#imports"
 import {
   Select,
   SelectContent,
